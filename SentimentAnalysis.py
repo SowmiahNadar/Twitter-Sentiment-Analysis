5f92506fc01e7a21fdd79f853c5578b7b749a917
Sowmiah <91007841+SowmiahNadar@users.noreply.github.com>
2022-12-04 00:28:50 +0530

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 pip install textblob
-
-
-# In[11]:
-
-
 pip install tweepy
-
-
-# In[12]:
-
-
+pip install 
 from textblob import TextBlob
 import sys
 import tweepy
